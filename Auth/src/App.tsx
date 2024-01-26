4a6e6bc0a2a46f4d595b0ad184880335974e433d
Suprakash Gorai <92419286+suprakash-144@users.noreply.github.com>
2024-01-27 00:17:03 +0530

    Context updated

--- Auth/src/App.tsx
+++ Auth/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Router from './Routes/Router';
+import {Appwriteprovider} from './appwrite/AppwriteCintext';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaProvider>
-      <Router />
-    </SafeAreaProvider>
+    <Appwriteprovider>
+      <SafeAreaProvider>
+        <Router />
+      </SafeAreaProvider>
+    </Appwriteprovider>
   );
 }
 export default App;
